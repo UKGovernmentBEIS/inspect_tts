dc9363f35e1e3f6a2cbee983b0b717e85317cb01
Sam.Deverett <Sam.Deverett@dsit.gov.uk>
2025-09-14 21:14:11 +0000

    Expose `AudioFormat`.

--- src/inspect_tts/__init__.py
+++ src/inspect_tts/__init__.py
@@ -1,7 +1,9 @@
 """An Inspect extension for text-to-speech."""
 
+from inspect_tts._audio import AudioFormat
 from inspect_tts._text_to_speech import text_to_speech
 
 __all__ = [
+    "AudioFormat",
     "text_to_speech",
 ]
